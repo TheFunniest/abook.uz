3ce4d61c6a83b00116d972787178890125b0258f
Doniyor Rajabov <donickuzb@gmail.com>
2020-08-26 12:15:39 +0500

    add route to index page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Route, Switch } from "react-router-dom";
+
+// PAGES
+import Index from "./pages/index";
 
 function App() {
   return (
     <div className="App">
-      abook
+      <Switch>
+        <Route path="/" exact>
+          <Index />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndexPage = () => {
+    return (
+        <div>
+            123
+        </div>
+    )
+}
+
+export default IndexPage
