09f383a1eadbceb27d820768ca304c6802582c41
Doniyor Rajabov <donickuzb@gmail.com>
2020-09-01 14:28:12 +0500

    Updates

---
+++ precache-manifest.0f094c521943aa88bbce51b4e93920de.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf1ecef1868a7a86d303d8700166a57",
+    "url": "/TheFunniest/abook.uz/index.html"
+  },
+  {
+    "revision": "c26493dc4ec85040ae2e",
+    "url": "/TheFunniest/abook.uz/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "76042620298739b70569",
+    "url": "/TheFunniest/abook.uz/static/css/main.2ff004da.chunk.css"
+  },
+  {
+    "revision": "c26493dc4ec85040ae2e",
+    "url": "/TheFunniest/abook.uz/static/js/2.42d3bcc2.chunk.js"
+  },
+  {
+    "revision": "a597e43a38483d067e3d5d713a48f9b9",
+    "url": "/TheFunniest/abook.uz/static/js/2.42d3bcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76042620298739b70569",
+    "url": "/TheFunniest/abook.uz/static/js/main.9b09ccd1.chunk.js"
+  },
+  {
+    "revision": "675a5bc4140c9b14b6d7",
+    "url": "/TheFunniest/abook.uz/static/js/runtime-main.54a648bc.js"
+  },
+  {
+    "revision": "394e11dc0c92b5c950534cc78ac17ff8",
+    "url": "/TheFunniest/abook.uz/static/media/app-store.394e11dc.svg"
+  },
+  {
+    "revision": "3115d748fbcf48055762241cd4eb1a39",
+    "url": "/TheFunniest/abook.uz/static/media/arrow_down.3115d748.svg"
+  },
+  {
+    "revision": "2ce35b9cc1a364b24afb647abfb43dbb",
+    "url": "/TheFunniest/abook.uz/static/media/audio.2ce35b9c.svg"
+  },
+  {
+    "revision": "01796d35eea8b0e7efc7e622a3e4ffbd",
+    "url": "/TheFunniest/abook.uz/static/media/banner_bookview.01796d35.png"
+  },
+  {
+    "revision": "ae293b928eb01da9ca21cfeb72a77f13",
+    "url": "/TheFunniest/abook.uz/static/media/book_img.ae293b92.png"
+  },
+  {
+    "revision": "3e0ce95de650f6327059dc7aa2017824",
+    "url": "/TheFunniest/abook.uz/static/media/books.3e0ce95d.svg"
+  },
+  {
+    "revision": "a7c8c892ef302e8bbad784cc2560b744",
+    "url": "/TheFunniest/abook.uz/static/media/dropdown_arrow.a7c8c892.svg"
+  },
+  {
+    "revision": "482f250095e822e988b541108d4b8a95",
+    "url": "/TheFunniest/abook.uz/static/media/genres.482f2500.svg"
+  },
+  {
+    "revision": "48b0e1ed32a464ddcd7d365dce1b7444",
+    "url": "/TheFunniest/abook.uz/static/media/glasses.48b0e1ed.svg"
+  },
+  {
+    "revision": "e77733fa49367fd9d0186974f1037493",
+    "url": "/TheFunniest/abook.uz/static/media/google-play.e77733fa.svg"
+  },
+  {
+    "revision": "90e2968b59d5fc6bea8cbace6c8ae2d7",
+    "url": "/TheFunniest/abook.uz/static/media/headphones.90e2968b.svg"
+  },
+  {
+    "revision": "7b07e7ecd9425aa1ec9546e7ba3bac93",
+    "url": "/TheFunniest/abook.uz/static/media/logo-dark.7b07e7ec.svg"
+  },
+  {
+    "revision": "18dceb86b841f0819b8f040e4805fcd8",
+    "url": "/TheFunniest/abook.uz/static/media/logo-light.18dceb86.svg"
+  },
+  {
+    "revision": "53b9a988676886deaf038226c9f35496",
+    "url": "/TheFunniest/abook.uz/static/media/mainSectionBg.53b9a988.jpg"
+  },
+  {
+    "revision": "cd26d65ce98ca95cb1feaee9205a9701",
+    "url": "/TheFunniest/abook.uz/static/media/order-about-bg.cd26d65c.svg"
+  },
+  {
+    "revision": "31350aa2ac63b0d744d35a6e5b0bc801",
+    "url": "/TheFunniest/abook.uz/static/media/phones.31350aa2.svg"
+  },
+  {
+    "revision": "5964b807690d3f53da0a002530ca5085",
+    "url": "/TheFunniest/abook.uz/static/media/romeo&juliett.5964b807.svg"
+  },
+  {
+    "revision": "7d063f236347db5de511aea635a13814",
+    "url": "/TheFunniest/abook.uz/static/media/searchIcon.7d063f23.svg"
+  },
+  {
+    "revision": "e18f53eb4ee4fb680b1b8b815eeb14df",
+    "url": "/TheFunniest/abook.uz/static/media/sells.e18f53eb.svg"
+  },
+  {
+    "revision": "4e42f90effb635ed0079df180a59deeb",
+    "url": "/TheFunniest/abook.uz/static/media/support.4e42f90e.svg"
+  },
+  {
+    "revision": "92c789cef991d5b920a7f73312906517",
+    "url": "/TheFunniest/abook.uz/static/media/users.92c789ce.svg"
+  }
+]);
