7a03987427c77f6f0005951fe23beae96bc2ddc8
Doniyor Rajabov <donickuzb@gmail.com>
2020-09-05 16:45:49 +0500

    add audio icon to book card

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,8 +3,9 @@ import "./styles.scss";
 import Rating from "@material-ui/lab/Rating";
 import Skeleton from "@material-ui/lab/Skeleton";
 import romeo from "../../assets/images/romeo&juliett.svg";
+import { ReactComponent as Headphones } from "../../assets/images/headphones_dark.svg";
 
-const BooksCard = ({ loading, mb }) => {
+const BooksCard = ({ loading, mb, audio }) => {
   return (
     <div
       className="card"
@@ -13,17 +14,20 @@ const BooksCard = ({ loading, mb }) => {
       }}
     >
       {loading ? (
-        <Skeleton  height={178} />
+        <Skeleton height={178} />
       ) : (
         <div className="card-image">
           <img src={romeo} alt="romeo and juliett" />
+          <div className="card-audio">
+            <Headphones />
+          </div>
         </div>
       )}
       <p className="card-name">
         {loading ? <Skeleton width={117} height={17} /> : "Romeo & Juliett"}
       </p>
       <p className="card-author">
-        {loading ? <Skeleton  height={17} /> : "William Shakespare"}
+        {loading ? <Skeleton height={17} /> : "William Shakespare"}
       </p>
       <p className="card-genre">
         {loading ? (
